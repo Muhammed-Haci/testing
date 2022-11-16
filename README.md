4b24f8a155352a008e906bc6de8f4514e51c7d4e
Muhammed-Haci <muhammed.hacilar@gmail.com>
2022-11-16 21:27:01 +0300

    u

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# testing
-this is a demo for testing things . . . 
+# Testing Demo
+
+version: 1.0.0
+
+URL Demo address: [Click Here](https://muhammed-haci.github.io/testing)
+
+author: Muhammed Haci
+
+Developed by: Muhammed Haci (me)
+
+website has been created using HTML, CSS, JS.
+
+the date of build: 01.08.2022
+
+the website pages:
+
+- Home page
+- XXXX Page
--- js/script.js
+++ js/script.js
@@ -1,9 +1,83 @@
-let text = document.querySelector("h2").textContent;
-let index = 1;
+let slider = document.querySelector(".slider");
+let slides = Array.from(document.querySelectorAll(".slider > figure"));
+let prev = document.querySelector(".prev");
+let next = document.querySelector(".next");
+let current = 0;
+let slidesCount = slides.length - 1;
 
-function writeText() {
-  document.querySelector("h2").innerHTML = text.slice(0, index);
-  index++;
-  if (index > text.length) index = 1;
+// create the indicaters items:
+for (let i = 0; i <= slidesCount; i++) {
+  let bullet = document.createElement("li");
+  bullet.setAttribute("data-index", i);
+  if (i === 0) {
+    bullet.classList.add("active");
+  }
+  document.querySelector(".indicators").appendChild(bullet);
 }
-setInterval(writeText, 200);
+
+// select the indicators items:
+let indicators = Array.from(document.querySelectorAll(".indicators li"));
+
+// ----------- prev button function -----------:
+prev.addEventListener("click", () => {
+  removeActive();
+
+  current--;
+
+  if (current < 0) {
+    current = slidesCount;
+  }
+
+  addActive();
+});
+
+// ----------- Next button function -----------:
+next.addEventListener("click", () => {
+  removeActive();
+
+  current++;
+
+  if (current > slidesCount) {
+    current = 0;
+  }
+
+  addActive();
+});
+
+// ------ indicators click function --------:
+indicators.forEach((indicator) => {
+  indicator.addEventListener("click", (e) => {
+    removeActive();
+    current = e.target.dataset.index;
+    addActive();
+  });
+});
+
+// ----------- Set autoplay function -----------:
+let clickNext = () => {
+  next.click();
+};
+
+let repeater = setInterval(clickNext, 5000);
+
+// ----------- Stop autoplay on hover function -----------:
+slider.addEventListener("mouseover", () => {
+  clearInterval(repeater);
+});
+
+// ----------- Set autoplay on mouse out function -----------:
+slider.addEventListener("mouseout", () => {
+  repeater = setInterval(clickNext, 5000);
+});
+
+// ----------- Remove Active Class function -----------:
+function removeActive() {
+  slides.forEach((slide) => slide.classList.remove("active"));
+  indicators.forEach((indicator) => indicator.classList.remove("active"));
+}
+
+// ----------- Add Active Class function -----------:
+function addActive() {
+  slides[current].classList.add("active");
+  indicators[current].classList.add("active");
+}
