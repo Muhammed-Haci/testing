087ac33c49a62104233c79837795ed6b8c0aad1f
Muhammed-Haci <muhammed.hacilar@gmail.com>
2022-11-20 16:07:42 +0300

    u

--- js/script.js
+++ js/script.js
@@ -1,32 +1,54 @@
-// // --- Toggle dark light mood ---:
+// --- Toggle dark light mood ---:
+const checkbox = document.querySelector("header [type='checkbox']");
+let localData = window.localStorage.getItem("Theme");
+let theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+  ? "Dark"
+  : "Light";
 
-// // Get the variables:
-// let themeSwicher = document.querySelector("#theme-swicher");
-// let themeIcon = document.querySelector("#theme-swicher .icon");
-// let localData = localStorage.getItem("theme");
+// check the Machine Theme:
+if (theme == "Dark") {
+  // check the localStorage Theme:
+  if (localData) {
+    if (localData == "Dark") {
+      checkbox.classList.add("checked");
+      checkbox.setAttribute("checked", "");
+    }
+  } else {
+    checkbox.classList.add("checked");
+    checkbox.setAttribute("checked", "");
+  }
+} else {
+  // check the localStorage Theme:
+  if (localData) {
+    if (localData == "Dark") {
+      checkbox.classList.add("checked");
+      checkbox.setAttribute("checked", "");
+    }
+  }
+}
 
-// // check the localStorage Theme:
-// if (localData) {
-//   localData == "light"
-//     ? (themeIcon.src = "images/moon.png")
-//     : (themeIcon.src = "images/sun.png");
-//   document.body.classList.add(localData);
-// }
+// Change The Theme function:
+checkbox.addEventListener("click", (e) => {
+  checkbox.classList.toggle("checked");
 
-// // Change The Theme function:
-// themeSwicher.addEventListener("click", () => {
-//   document.body.classList.toggle("dark");
-
-//   if (document.body.classList.contains("dark")) {
-//     themeIcon.src = "images/sun.png";
-//     localStorage.setItem("theme", "dark");
-//     document.body.classList.remove("light");
-//   } else {
-//     themeIcon.src = "images/moon.png";
-//     document.body.classList.add("light");
-//     localStorage.setItem("theme", "light");
-//   }
-// });
+  if (checkbox.classList.contains("checked")) {
+    checkbox.setAttribute("checked", "");
+    window.localStorage.setItem("Theme", "Dark");
+    // add the audio to the body:
+    const audio = document.createElement("audio");
+    audio.src = "https://b.top4top.io/m_25146mzva2.mp3";
+    audio.play();
+    audio.remove();
+  } else {
+    checkbox.removeAttribute("checked");
+    window.localStorage.setItem("Theme", "Light");
+    // add the audio to the body:
+    const audio = document.createElement("audio");
+    audio.src = "https://a.top4top.io/m_2514r403l1.mp3";
+    audio.play();
+    audio.remove();
+  }
+});
 
 // Add the copyright year to the footer:
 let year = document.querySelector("footer .year");
